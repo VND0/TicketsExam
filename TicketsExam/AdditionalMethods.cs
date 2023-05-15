b928eee3a9e7d7a940b1537c56981434fdc1e79b
user <varlamov577123@gmail.com>
2023-05-15 21:30:07 +0300

    ваптрьонбллбьерткыпуиафуеньгбшу па

--- TicketsExam/AdditionalMethods.cs
+++ TicketsExam/AdditionalMethods.cs
@@ -1,18 +1,18 @@
 ﻿class AdditionalMethods
 {
-    public static string[] StringArrayAppendWithString(string[] oldArray, string newElement)
+    public static string[] Append(string[] oldArray, string newElement)
     {
         string[] temp = oldArray;
         Array.Resize(ref temp, temp.Length + 1);
         temp[^1] = newElement;
         return temp;
     }
-    public static string[] StringArrayAppendWithArray(string[] oldArray, string[] newArray)
+    public static string[] Extend(string[] oldArray, string[] newArray)
     {
         string[] temp = oldArray;
         foreach (string element in newArray)
         {
-            temp = StringArrayAppendWithString(temp, element);
+            temp = Append(temp, element);
         }
         return temp;
     }
